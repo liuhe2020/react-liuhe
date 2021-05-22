bed027a33e0811d800895d5dbd08763358eeea50
Liu He <125852364@qq.com>
2021-05-22 03:14:11 +0100

    add new project

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -2,25 +2,45 @@ import "../styles/Cards.css";
 import CardItem from "./CardItem";
 
 function Cards() {
+  const projects = [
+    {
+      title: "BINGEDIN MOVIE APP",
+      text: "bINGEdIN is a movie app where users can view infos and watch trailers of trending movies over a variety of genres. It features a search functionality, and a watch list that stores user added movies in local storage. The app was built with React.js with styled components, utilising TMDB's APIs for movie data and YouTube for trailers.",
+      src: "img/bingedin.jpg",
+      url: "https://bingedin.netlify.app",
+      gitUrl: "https://github.com/liuhe2020/react-bingedin",
+    },
+    {
+      title: "YAAK CHATROOM",
+      text: "Yaak chatroom is an online chat app with multi-rooms support. Users can log in with Google or Facebook and chat to one another. It also supports emojis. Yaak was built with HTML, SASS and vanilla JavaScript, utilising Firebase for data storage and user authentication.",
+      src: "img/yaak.jpg",
+      url: "https://yaak-aa6a6.web.app",
+      gitUrl: "https://github.com/liuhe2020/yaak",
+    },
+    {
+      title: "LIU RIVER PHOTOGRAPHY",
+      text: "A photographer's website built using HTML, CSS and vanilla JavaScript with features including custom built gallery lightbox and client side form validation.",
+      src: "img/liu_river_photography.jpg",
+      url: "https://www.liuriver.co.uk",
+      gitUrl: "https://github.com/liuhe2020/liuriver",
+    },
+  ];
+
   return (
     <div id="projects">
       <div className="project-title-container">
         <h2>PROJECTS</h2>
       </div>
-      <CardItem
-        title="YAAK CHATROOM"
-        text="Yaak chatroom is an online chat app with multi-rooms support. Users can log in with Google or Facebook and chat to one another. It also supports emojis. Yaak was built with HTML, SASS and vanilla JavaScript, utilising Firebase for data storage and user authentication."
-        src="img/yaak.jpg"
-        url="https://yaak-aa6a6.web.app/"
-        gitUrl="https://github.com/liuhe2020/yaak"
-      />
-      <CardItem
-        title="LIU RIVER PHOTOGRAPHY"
-        text="A photographer's website built using HTML, CSS and vanilla JavaScript with features including custom built gallery lightbox and client side form validation."
-        src="img/liu_river_photography.jpg"
-        url="https://www.liuriver.co.uk"
-        gitUrl="https://github.com/liuhe2020/liuriver"
-      />
+      {projects.map(({ title, text, src, url, gitUrl }) => (
+        <CardItem
+          key={title}
+          title={title}
+          text={text}
+          src={src}
+          url={url}
+          gitUrl={gitUrl}
+        />
+      ))}
     </div>
   );
 }
