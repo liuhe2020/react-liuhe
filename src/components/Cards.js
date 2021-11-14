8e79853044b01e005dcd9af1c4fc5a1fdb7caecb
Liu He <lhe19@yahoo.co.uk>
2021-11-14 03:11:00 +0000

    change project link

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -7,7 +7,7 @@ function Cards() {
       title: 'PIN MY MAP',
       text: 'Pin My Map offers users the ability to record places they have visited using the Mapbox APIs. Users can register an account and simply pin locations on the world map, add details and photos, as well as share their personalised map page on social networks. It is built using React with Material UI and Styled Components, and powered by a Strapi back-end.',
       src: 'img/pin_my_map.jpg',
-      url: 'https://pinmymap.netlify.app/maps/1',
+      url: 'https://pinmymap.netlify.app',
       gitUrl: 'https://github.com/liuhe2020/react-pin-my-map-frontend',
     },
     {
