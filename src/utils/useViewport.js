c4b56673e25906b79469f051bf5f07627bf281d0
Liu He <lhe19@yahoo.co.uk>
2023-01-29 15:40:05 +0000

    fixed useEffect error

--- src/utils/useViewport.js
+++ src/utils/useViewport.js
@@ -16,7 +16,7 @@ const useViewport = () => {
     window.addEventListener('resize', handleResize);
 
     return () => window.removeEventListener('resize', handleResize);
-  }, []);
+  }, [handleResize]);
 
   return viewportWidth;
 };
